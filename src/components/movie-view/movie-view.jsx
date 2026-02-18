eafba5f0c3b49fdc01587c0795075a43e5c0e235
EnkeledaM <kelamustafaj@hotmail.com>
2026-02-18 17:44:37 +0100

    Update MovieView component

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,19 +1,28 @@
 export const MovieView = ({ movie, onBackClick }) => {
+  // nëse s’ka movie (për siguri), mos shfaq asgjë
+  if (!movie) return null;
+
   return (
     <div>
-      {/* Titulli */}
-      <h2>{movie.title}</h2>
+      <h1>{movie.title}</h1>
 
-      {/* Poster */}
-      <img src={movie.image} alt={movie.title} />
+      
+      {/* Poster image */}
+      <img
+        src={movie.image}
+        alt={movie.title}
+        style={{ width: "150px" }}
+      />
 
-      {/* Info bazë */}
+
+      {/* detaje */}
       <p><b>Description:</b> {movie.description}</p>
       <p><b>Genre:</b> {movie.genre}</p>
       <p><b>Director:</b> {movie.director}</p>
 
-      {/* Kthen mbrapsht te lista */}
+      {/* kthehu mbrapa */}
       <button onClick={onBackClick}>Back</button>
     </div>
   );
 };
+
