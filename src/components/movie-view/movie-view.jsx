1aab41f19c20b07b5b95a15552bd4854d7c42c61
EnkeledaM <kelamustafaj@hotmail.com>
2026-02-19 22:38:44 +0100

    Fix MovieView image fallback

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,31 +1,25 @@
+import placeholder from "../../assets/interstellar.jpeg";
 
 export const MovieView = ({ movie, onBackClick }) => {
   if (!movie) return null;
+
   return (
     <div>
       <h1>{movie.Title}</h1>
-        <p><b>ImagePath:</b> {movie.ImagePath}</p>
-          
+
       <img
         src={movie.ImagePath}
         alt={movie.Title}
         style={{ width: "150px" }}
         onError={(e) => {
-         e.target.src = "https://via.placeholder.com/150x220?text=No+Image";
-            }}
-        />
+          e.currentTarget.onerror = null; // mos hyjë në loop
+          e.currentTarget.src = placeholder;
+        }}
+      />
 
-      <p>
-        <b>Description:</b> {movie.Description}
-      </p>
-
-      <p>
-        <b>Genre:</b> {movie.Genre?.Name}
-      </p>
-
-      <p>
-        <b>Director:</b> {movie.Director?.Name}
-      </p>
+      <p><b>Description:</b> {movie.Description}</p>
+      <p><b>Genre:</b> {movie.Genre?.Name}</p>
+      <p><b>Director:</b> {movie.Director?.Name}</p>
 
       <button onClick={onBackClick}>Back</button>
     </div>
