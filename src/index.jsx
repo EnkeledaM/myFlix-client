7e02c6962335fdac5482d3384e2d8f8b6a0ef655
EnkeledaM <kelamustafaj@hotmail.com>
2026-02-22 21:57:46 +0100

    Fix index root initialization

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,14 @@
 import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
 import "./index.scss";
+import Container from "react-bootstrap/Container";
+
+root.render(
+  <Container>
+    <App />
+  </Container>
+);
+
 
 const App = () => {
   return <MainView />;
