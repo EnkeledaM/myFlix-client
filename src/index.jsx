04f05734ed529f01b12c41c82093256fc8a1bce0
EnkeledaM <kelamustafaj@hotmail.com>
2026-02-22 22:05:51 +0100

    Fix root render and add Bootstrap container

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,14 @@
-
-
 import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
 import "./index.scss";
 import Container from "react-bootstrap/Container";
 
-root.render(
-  <Container>
-    <App />
-  </Container>
-);
-
-
 const App = () => {
-  return <MainView />;
+  return (
+    <Container>
+      <MainView />
+    </Container>
+  );
 };
 
 const container = document.querySelector("#root");
